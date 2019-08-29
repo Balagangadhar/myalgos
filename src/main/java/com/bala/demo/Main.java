480b4a93ad57bd3deb28fcd96bc73a63bb12677e
Bala <balagangadhar85@gmail.com>
2019-08-29 09:54:33 +0530

    Object bAED MERGESORT

--- src/main/java/com/bala/demo/Main.java
+++ src/main/java/com/bala/demo/Main.java
@@ -5,8 +5,20 @@ public class Main {
 
     public static void main(String[] args) {
 
+//        testMergeSort();
+        testOMergeSort();
+    }
+
+    public static void testMergeSort() {
         MergeSort msort = new MergeSort();
         int[] data = new int[]{2, 1, 3, 6, 9, 5, 4,};
+        msort.sort(data, 0, data.length - 1);
+        msort.print(data);
+    }
+
+    public static void testOMergeSort() {
+        OMergeSort<String> msort = new OMergeSort();
+        String[] data = new String[]{"a", "c", "b", "d", "ab", "bd", "ad"};
         msort.sort(data, 0, data.length - 1);
         msort.print(data);
     }
---
+++ src/main/java/com/bala/demo/OMergeSort.java
@@ -0,0 +1,60 @@
+package com.bala.demo;
+
+import java.util.Arrays;
+
+public class OMergeSort<T extends Comparable<T>> {
+
+
+    public void sort(T[] A, int p, int r) {
+        if (p < r) {
+            int q = (p + r) / 2;
+//            sort(A, p, q);
+//            sort(A, q + 1, r);
+            merge(A, p, q, r);
+        }
+    }
+
+
+    private void merge(T[] A, int p, int q, int r) {
+
+        int n1 = q - p + 1;
+        int n2 = r - q;
+
+        Object L[] = new Object[n1 + 1];
+        Object R[] = new Object[n2 + 1];
+
+        for (int i = 0; i < n1; i++) {
+            L[i] = A[p + i];
+        }
+
+        for (int j = 0; j < n2; j++) {
+            R[j] = A[q + j + 1];
+        }
+
+        int i = 0, j = 0;
+        for (int k = p; k <= r; k++) {
+
+            T Li = (T) L[i];
+            T Rj = (T) R[j];
+            if (Li != null && Rj != null) {
+                break;
+            }
+            if (Li.compareTo(Rj) < 0) {
+                A[k] = Li;
+                i++;
+            } else {
+                A[k] = Rj;
+                j++;
+            }
+        }
+
+    }
+
+    public void print(T[] A) {
+        if (A != null && A.length > 0) {
+            Arrays.stream(A).forEach(k -> System.out.print(k + " - "));
+        }
+    }
+
+
+}
